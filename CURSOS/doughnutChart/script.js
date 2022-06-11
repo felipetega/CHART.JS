8f120d7fdd38de4defbdce6c4265d59b5769d839
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-06-11 14:12:10 -0300

    main

--- CURSOS/doughnutChart/script.js
+++ CURSOS/doughnutChart/script.js
@@ -1,5 +1,13 @@
+//COLORS
 const BACKGROUND_COLORS = ['rgba(255, 99, 132, 0.2)','rgba(54, 162, 235, 0.2)','rgba(255, 206, 86, 0.2)','rgba(75, 192, 192, 0.2)','rgba(153, 102, 255, 0.2)','rgba(255, 159, 64, 0.2)'];
 const BORDER_COLORS = ['rgba(255, 99, 132, 1)','rgba(54, 162, 235, 1)','rgba(255, 206, 86, 1)','rgba(75, 192, 192, 1)','rgba(153, 102, 255, 1)','rgba(255, 159, 64, 1)'];
+
+//DATA
+var data=[120,40,98,63]
+//SUM DATA VALUES
+
+var getTotal = ()=>{sum=data.reduce((a, b) => a + b, 0);return (`Total de horas: ${sum}`)}
+//CHART
 
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
@@ -8,7 +16,7 @@ const myChart = new Chart(ctx, {
         labels: ["Curso em Vídeo: Python","Curso em Vídeo: JavaScript","DIO: Spread Fullstack Developer", "DIO: Órbi Web Games Developer"],
         datasets: [{
             label: '# of Votes',
-            data: [120,40,98,63],
+            data: data,
             backgroundColor: BACKGROUND_COLORS,
             borderColor: BORDER_COLORS,
             borderWidth: 3,
@@ -27,7 +35,7 @@ const myChart = new Chart(ctx, {
         },
         subtitle: {
           display: true,
-          text: 'Total de horas: 321',
+          text: getTotal,
           font:{
             size: 25
           }
